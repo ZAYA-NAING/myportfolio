0140a1b8d48d31e1b29a58b6484b24ddfdb6f0c5
ZAYA-NAING <zayanaing.scm@gmail.com>
2024-07-21 00:56:48 +0630

    Added: english language function

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
+import { languagesInfo } from "./scripts/language-boilerplate.js";
 $(function () {
-    AOS.init();
+  AOS.init();
 
-    $('.nav-link').onePageNav({
-        wrapper: '#one-page-nav',
-        navStart: window.innerHeight / 2.5,
+  $(".nav-link").onePageNav({
+    wrapper: "#one-page-nav",
+    navStart: window.innerHeight / 2.5,
+  });
+  $(".nav img").click(function () {
+    $(".nav").toggleClass("sp-nav");
+  });
+
+  $(".lang-btn").click(function (event) {
+    // Get event target class list of contain with `en` and set the language variable
+    let language = event.target.classList.contains("en") ? "en" : "jp";
+    // Get all keys (key)
+    const keys = Object.keys(languagesInfo.languages);
+
+    // Append to the english language `same CSS class name` with all same keys
+    keys.forEach((key) => {
+      $(`.${key}`).html(`${languagesInfo.languages[key][language]}`);
     });
-    $('.nav img').click(function () {
-        $('.nav').toggleClass('sp-nav');
-    });
+  });
 });
--- scripts/one-page-nav.js
+++ scripts/one-page-nav.js
@@ -1,42 +1,49 @@
 (function ($) {
+  $.fn.onePageNav = function (options) {
+    const settings = $.extend(
+        {
+          activeClass: "active",
+          wrapper: "", // Nav wrapper selector for scroll effect
+          speed: 900, // animation speed
+          navStop: 50, // stop before top
+          navStart: 200, // change class before navstart pixel
+        },
+        options
+      ),
+      navLink = $(this),
+      targetOffsetTops = [];
 
-    $.fn.onePageNav = function (options) {
+    navLink.on("click", clickScroll);
 
-        const settings = $.extend({
-                activeClass: 'active',
-                wrapper: '',  // Nav wrapper selector for scroll effect
-                speed: 900,   // animation speed
-                navStop: 50,  // stop before top
-                navStart: 200, // change class before navstart pixel
-            }, options),
-            navLink = $(this),
-            targetOffsetTops = [];
-
-        navLink.on('click', clickScroll);
-
-        if (settings.wrapper) {
-            navLink.each(function (index, navLinkElement) {
-                targetOffsetTops.push($($(navLinkElement).attr('href')).offset().top);
+    if (settings.wrapper) {
+      navLink.each(function (index, navLinkElement) {
+        targetOffsetTops.push($($(navLinkElement).attr("href")).offset().top);
+      });
+      $(window)
+        .on("scroll", function () {
+          const windowTop = $(window).scrollTop();
+          setTimeout(function () {
+            $(targetOffsetTops).each(function (index, targetOffsetTops) {
+              if (windowTop > targetOffsetTops - settings.navStart) {
+                navLink
+                  .removeClass(settings.activeClass)
+                  .eq(index)
+                  .addClass(settings.activeClass);
+              }
             });
-            $(window).on('scroll', function () {
-                const windowTop = $(window).scrollTop();
-                setTimeout(function() {
-                    $(targetOffsetTops).each(function (index, targetOffsetTops) {
-                        if (windowTop > targetOffsetTops - settings.navStart) {
-                            navLink.removeClass(settings.activeClass)
-                                .eq(index).addClass(settings.activeClass);
-                        }
-                    });
-                }, 250);
-            }).trigger('scroll');
-        }
+          }, 250);
+        })
+        .trigger("scroll");
+    }
 
-        function clickScroll(event) {
-            event.preventDefault();
-            $('html, body').animate({
-                scrollTop: $($(this).attr('href')).offset().top - settings.navStop
-            }, settings.speed);
-        }
-    };
-
-}(jQuery));
+    function clickScroll(event) {
+      event.preventDefault();
+      $("html, body").animate(
+        {
+          scrollTop: $($(this).attr("href")).offset().top - settings.navStop,
+        },
+        settings.speed
+      );
+    }
+  };
+})(jQuery);
