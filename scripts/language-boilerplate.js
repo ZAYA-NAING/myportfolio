bf461991d36215df131362e20a0a1090e96308d4
ZAYA-NAING <zayanaing.scm@gmail.com>
2024-07-21 09:26:49 +0630

    Fixed: wrong sentences and minor collections

--- scripts/language-boilerplate.js
+++ scripts/language-boilerplate.js
@@ -9,15 +9,14 @@ const languages = {
             Unitの単体テスト、テストケース作成、障害対応なの経験を持っています。
             コードを実装する時に他のチームメンバーが分かりやすい、メンテナンスしやすいコードをいつも心掛けています。
             今後はウェブ開発者もっと学びつづ、スマホアプリ開発にも挑戦して行きたいです。`,
-    en: `I develop SPAs using Javascript, Typescript, and PHP, with Angular for the front end and Laravel for the back end.
-I have experience in front-end and back-end coding implementation, Scrum development, PHP
-Unit unit testing, test case creation, and troubleshooting.
-When implementing code, I always try to write code that is easy for other team Members to understand and maintain.
-In the future, I would like to continue learning more as a web developer and try my hand at smartphone app development.`,
+    en: `I develop SPA using Javascript, Typescript, and PHP, with Angular for the front-end and Laravel for the back-end.
+          I have experience in front-end and back-end coding implementation, Scrum development, PHP Unit testing, test case creation and troubleshooting.
+          When implementing code, I always try to write code that is easy for other team Members to understand and maintain.
+          In the future, I would like to learn more about web development and challenge smartphone app development.`,
   },
   project_1_title: {
     jp: "01. 基幹業務システム",
-    en: "01. Core business systems",
+    en: "01. Core business system",
   },
   project_1_description: {
     jp: `受注から印刷工程、出荷までの流れで、ペーパーレスを目的としたシステムです。 私は主にフロントエンドとバックエンドの開発をして、単体テスト、画面上テスト、バグ対応などを担当しています。受注から印刷工程出荷までの流れで、 ペーパーレスを目的としたシステムです。`,
@@ -25,7 +24,7 @@ In the future, I would like to continue learning more as a web developer and try
   },
   project_1_label_1: {
     jp: "開発開始: 2018年 10月",
-    en: "Start of development: October 2018",
+    en: "Start date of development: October 2018",
   },
   project_1_label_2: {
     jp: "メンバー: 7人",
@@ -33,15 +32,15 @@ In the future, I would like to continue learning more as a web developer and try
   },
   project_2_title: {
     jp: "02. パートさん管理システム",
-    en: "Core business systems",
+    en: "02. Part-timer management system",
   },
   project_2_description: {
-    jp: `受注から印刷工程、出荷までの流れで、ペーパーレスを目的としたシステムです。 私は主にフロントエンドとバックエンドの開発をして、単体テスト、画面上テスト、バグ対応などを担当しています。受注から印刷工程出荷までの流れで、 ペーパーレスを目的としたシステムです。`,
-    en: `The system is aimed at going paperless from the flow of orders, through the printing process, to shipping. I mainly develop the front-end and back-end, and am in charge of unit testing, on-screen testing, and bug response. The system is aimed at going paperless from the flow of orders, through the printing process, to shipping.`,
+    jp: `パートさんの出勤・退勤、業務日報、シフト、時給予算などが管理できるシステムです。私は主にER図作成、レスポンス定義、フロントエンド、バックエンド、単体テスト、画面上テスト、バグ対応などを担当しています。`,
+    en: `It is a system that can manage part-timer's attendance, retirement, daily work schedule, shift, and salary budget. I am mainly responsible for ER diagram creation, response definition, front-end, back-end, unit testing, on-screen testing, and bug response.`,
   },
   project_2_label_1: {
     jp: "開発開始: 2019年 05月",
-    en: "Start of development: May 2019",
+    en: "Start date of development: May 2019",
   },
 
   project_2_label_2: {
@@ -51,15 +50,15 @@ In the future, I would like to continue learning more as a web developer and try
 
   project_3_title: {
     jp: "03. 社内FAQシステム",
-    en: "03. Internal FAQ system",
+    en: "03. Company's FAQ system",
   },
   project_3_description: {
-    jp: `EC サイトの商品、注文などに関するお客様の問い合わせを対応するための 社内用FAQシステムです。 設計から開発、フロントエンド、バックエンド、単体テスト、画面上テスト、バグ対応などを担当しています。`,
-    en: `03. In-house FAQ system This is an in-house FAQ system for handling customer inquiries regarding products, orders, etc. on the EC site. I am responsible for everything from design to development, front-end, back-end, unit testing, on-screen testing, and bug response.`,
+      jp: `EC サイトの商品、注文などに関するお客様の問い合わせを対応するための 社内用FAQシステムです。 設計から開発、フロントエンド、バックエンド、単体テスト、画面上テスト、バグ対応などを担当しています。`,
+      en: `Company's FAQ system is a frequent answer and question system for handling customer inquiries regarding products, orders, etc. on the EC site. I am responsible for everything from design to development, front-end, back-end, unit testing, on-screen testing, and bug response.`,
   },
   project_3_label_1: {
     jp: "開発開始: 2021年 06月",
-    en: "Start of development: June 2021",
+    en: "Start date of development: June 2021",
   },
   project_3_label_2: {
     jp: "メンバー: 3人",
@@ -67,15 +66,15 @@ In the future, I would like to continue learning more as a web developer and try
   },
   project_4_title: {
     jp: "04. 発注管理システム",
-    en: "04. Order Management System",
+    en: "04. Outstanding Order Management System",
   },
   project_4_description: {
     jp: `自社製造品以外の製品をパートナー企業に発注する受注管理システムです。主にフロントエンド、バックエンド、単体テスト、画面テスト、API連携などを実装します。`,
-    en: `This is an order management system that orders products that are not manufactured by our company to partner companies. It mainly implements front-end, back-end, unit tests, on-screen tests, and API integration.`,
+    en: `This is an outstanding order management system that orders products that are not manufactured by our company to partner companies. It mainly implements front-end, back-end, unit testing, on-screen testing, and API integration.`,
   },
   project_4_label_1: {
     jp: "開発開始: 2021年 06月",
-    en: "Start of development: June 2021",
+    en: "Start date of development: June 2021",
   },
   project_4_label_2: {
     jp: "メンバー: 7人",
